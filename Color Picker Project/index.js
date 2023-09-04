1b8cc601445c284d18aed99c38a208858d8537f5
oladimeji <oladimeji.abaniwonnda@pau.edu.ng>
2023-09-04 12:41:15 +0100

    ()

--- Color Picker Project/index.js
+++ Color Picker Project/index.js
@@ -1,9 +1,17 @@
-const colorInput =  document.querySelector('#currentcolor');
 const body = document.querySelector('body');
+const colorInput =  document.querySelector('#currentcolor');
 const displayColor = document.querySelector('#displaycolor')
+const opacityInput = document.querySelector('#currentopacity')
+const displayOpacity = document.querySelector("#displayopacity")
+
 displayColor.innerText = colorInput.value
 colorInput.addEventListener('input', function(e){ 
     body.style.backgroundColor = colorInput.value;
     displayColor.innerText = colorInput.value
+})
+displayOpacity.innerText = opacityInput.value
+opacityInput.addEventListener('input', function(e){ 
+    body.style.opacity = opacityInput.value;
+    displayOpacity.innerText = opacityInput.value
 })
  
